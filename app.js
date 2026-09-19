657909c7ada71bd5b3392ba87485578cbf9bdc70
owaisahmed2303 <owaisahmed2303@gmail.com>
2026-09-19 09:32:53 +0500

    third

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 var display=document.getElementById("display");
+
+display.value=0;
+
 var display2=document.getElementById('display2');
+
 function addToDisplay(input){
- display.value += input;
-}
-function Clear(){
-  if(display !=''){
-          display.value='';
+  if(display.value=='0'){
+    display.value=input;
   }
+  else{
+    display.value += input;
+  
+}
+
+}
+var count=0;
+function Clear(){
+  
+ if(count==0){
+  display.value='0';
+  count=1;
+ }
+else{
+  display2.value='0';
+  count=0;
+}
+        
 }
 function calculate(){
+  
+  
   display2.value=display.value;
    display.value=eval(display.value);
+  
    
 }
 function deleteValue(){
-   display.value = display.value.slice(0, -1);
+  display.value = display.value.slice(0, -1);
+  if(display.value==''){
+     display.value=0;
+  }
 }
 
